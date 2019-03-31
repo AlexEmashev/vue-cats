ba71362292022a67b21f681d5cd4e52a2dcf2a4b
Alexander Emashev <alex.emashev@gmail.com>
2019-03-31 22:59:28 +0300

    Update readme and about

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-# vue-cats
+![Logo](public/favicon.svg)
 
-## Project setup
-```
-npm install
-```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+# About
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Description
 
-### Lints and fixes files
-```
-npm run lint
-```
+Vue Cats is a web-app featuring still and GIF images of amazing cats. Photos provided by https://thecatapi.com.
+
+The project has been created to explore web-framework Vue.js https://vuejs.org and source files available under The MIT License on project's Github page. Feel free to explore, fork and create your own versions based on the source-codes of the app.
+
+## Demo page
+
+
+Please see [the demo page](https://alexemashev.github.io/vue-cats/).
+
+
+## License
+
+The application developed under [The MIT License](https://opensource.org/licenses/MIT).
+
+**Author:** [Alexander Emashev](https://github.com/AlexEmashev).
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About the project</h1>
+    <section class="about-text">
+      <p>
+        Vue Cats is a web-app featuring still and GIF images of amazing cats. Photos provided by
+        <a
+          href="https://thecatapi.com"
+        >https://thecatapi.com</a>.
+      </p>
+      <p>
+        The project has been created to explore web-framework Vue.js
+        <a
+          href="https://vuejs.org"
+        >https://vuejs.org</a> and source files available under The MIT License on project's
+        <a href="https://github.com/AlexEmashev/vue-cats">Github page</a>. Feel free to explore, fork and create your own versions based on the source-codes of the app.
+      </p>
+    </section>
   </div>
 </template>
+<style lang="scss" scoped>
+  h1 {
+    font-size: 24px;
+    letter-spacing: 0.3px;
+    padding: 30px 0;
+  }
+  .about-text {
+    text-align: left;
+    font-size: 16px;
+    p {
+      padding-bottom: 15px;
+      word-spacing: 0.05em;
+      line-height: 1.2em;
+    }
+  }
+</style>
+
